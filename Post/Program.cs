836d6b85e73e3fa6212d8666e789d44b608f095b
Ihor <igormykhno@gmail.com>
2021-10-20 10:25:59 +0300

    Create Post class, and methods Like, GetLikesNumber

---
+++ Post/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        class Post
+        {
+            private int likes;
+            private string[] comments;
+            public void Like()
+            {
+                likes += 1;
+            }
+            public int GetLikesNumber()
+            {
+                return likes;
+            }
+            public void LeaveComment()
+        }
+    }
+
+}
