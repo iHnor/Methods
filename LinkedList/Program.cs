4c6a54426540453494839dd45290df48af5725fe
Ihor <igormykhno@gmail.com>
2021-10-21 11:43:50 +0300

    Final version of Linked List program

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -34,32 +34,69 @@ namespace Structures
                 list.size++;
                 list.tail = newNode;
             }
+            public void Unshift(LinkedList list, String value)
+            {
+                ListNode newNode = new ListNode(value);
+                if (list.tail == null)
+                {
+                    list.head = newNode;
+                    list.tail = newNode;
+                }
+                else
+                {
+                    newNode.nextNode = list.head;
+                    list.head = newNode;
+                }
+                list.size++;
+            }
             public int getSize()
             {
                 return size;
             }
             public void allValue(LinkedList list)
             {
-                LinkedList tmp = list;
-                int size = getSize();
-                for (int i = 0; i < size; i++)
+                ListNode tmp = list.head;
+                while (tmp != null)
                 {
-                    System.Console.WriteLine(tmp.head.value);
+                    System.Console.WriteLine(tmp.value);
+                    tmp = tmp.nextNode;
                 }
             }
-            public void Unshift(LinkedList list, String value)
+            public void changeValue(LinkedList list, int id, string newValue)
             {
-                ListNode newNode = new ListNode(value);
-                if (list.head == null)
+                ListNode tmp = list.head;
+                if (0 < id && id < getSize())
                 {
-                    tail.nextNode = newNode;
+                    for (int i = 1; i != id; i++)
+                    {
+                        tmp = tmp.nextNode;
+                    }
+                    tmp.value = newValue;
+                }
+                else
+                    System.Console.WriteLine("An element with this id does not exist!");
+            }
+
+            public void removeElement(LinkedList list, int id)
+            {
+                ListNode total = list.head;
+                ListNode before = null;
+                if (id == 1)
+                {
+                    list.head = total.nextNode;
                 }
                 else
                 {
-                    list.head = newNode;
+                    for (int i = 1; i != id; i++)
+                    {
+                        before = total;
+                        total = total.nextNode;
+                    }
+                    if (list.tail != null)
+                        before.nextNode = total.nextNode;
+                    else
+                        before.nextNode = null;
                 }
-                list.size++;
-                list.tail = newNode;
             }
         }
         static void Main(string[] args)
@@ -67,10 +104,19 @@ namespace Structures
             LinkedList list = new LinkedList();
             list.Append(list, "First");
             list.Append(list, "Second");
-            list.Unshift(list, "Third");
+            list.Unshift(list, "New");
             list.Append(list, "Third");
 
-            list.allValue(list);
+            // list.allValue(list);
+
+            list.changeValue(list, 3, "Change Value");
+            // System.Console.WriteLine();
+            // list.allValue(list);
+
+            // System.Console.WriteLine();
+            list.removeElement(list, 4);
+            // list.allValue(list);
+
         }
     }
 }
